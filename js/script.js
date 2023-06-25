3057141edf21f7e033b8a20746a2b0a09b7369c2
VLuca997 <kulertoast997@gmail.com>
2023-06-25 18:34:37 +0200

    ADD Human code JS

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/*
+    
+    1) Definiamo la funzione per:
+        - numeri casuali
+        - numeri unici in modo da non ripetersi nel gioco 
+    2) inizializziamo il button, per la generazione de inumeri casuali su schermo
+    3) stampiamo i numeri randomici in pagina
+    4) con un timer rimuoviamo i numeri in pagina
+    5) facciamo comparire dei prompt per richiederci l'inserimento di quei 5 numeri, uno per uno.
+        - convertiamo i valori in numeri interi
+    6) funzione per lo start, unita al button, avvia il gioco tramite l'evento click sul button
+    7) blocchiamo il pulsante button durante il gioco per non avere problemi tipo: avvii multipli.
+    8) si genererà un array di numeri casuali tra 1 e 60
+        -facciamo visualizzare i numeri sul DOM
+        - dopo 10 secondi i numeri spariscono, e dopo altri 5s comprarirà il prompt per richiedere un numer oalla volta
+    9) viene mostrato l'ultimo prompt con quanti numeri indovinati abbiamo finito il gioco.
+*/
